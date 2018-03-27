d2be3c9ab4ca21a76de009cd9fb7ca94a452c54c
Aaden Sah <deltatechaadensah@gmail.com>
2018-03-27 17:30:14 +0545

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ContactusPage } from '../pages/contactus/contactus';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  // rootPage: any = HomePage;
-  rootPage: any = ContactusPage;
+  
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
